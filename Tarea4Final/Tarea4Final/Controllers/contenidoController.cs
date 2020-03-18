7dd8f0102a4e088a14f7f1a5288ae20faddd83e8
LauraMaria <lauraqh2009@hotmail.com>
2020-03-18 11:24:23 -0400

    Se creo el portal de conversiones y se agrego el filtro de contendios, se puedieron actualizar las noticias y el portal de musica

---
+++ Tarea4Final/Tarea4Final/Controllers/contenidoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tarea4Final.Controllers
+{
+    public class contenidoController : Controller
+    {
+        // GET: contenido
+        public ActionResult index()
+        {
+            return View();
+        }
+
+        public ActionResult noticias()
+        {
+            return View();
+        }
+
+        public ActionResult musica()
+        {
+            return View();
+        }
+
+        public ActionResult peliculas()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tarea4Final/Tarea4Final/Controllers/conversionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tarea4Final.Controllers
+{
+    public class conversionController : Controller
+    {
+        // GET: conversion
+        public ActionResult conversiones()
+        {
+            return View();
+        }
+    }
+}
