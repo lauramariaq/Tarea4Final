8e535f5a675e37048f534378361d30660055b315
LauraMaria <lauraqh2009@hotmail.com>
2020-03-18 11:39:37 -0400

    se actualizo el mapeo de las vistas para organizar como serian vistas y se conecten entre si

--- Tarea4Final/Tarea4Final/App_Start/RouteConfig.cs
+++ Tarea4Final/Tarea4Final/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Tarea4Final
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "contenido", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
